8a6cf568d09cebacaeb4f8858d0a3829be078ee1
apekshadumbre <93672377+apekshadumbre@users.noreply.github.com>
2026-03-30 16:00:19 +0530

    reverse number using while loop

---
+++ reverseno.c
@@ -0,0 +1,19 @@
+//reverse number using loop
+
+#include<stdio.h>
+int main()
+{
+   int n,i=1;
+   
+   printf("Enter the number:");
+   scanf("%d",&n);
+   
+   while(n>=i)
+   {
+   	 printf("%d\n",n);
+   	 n--;
+   	 
+   }
+
+	return 0;
+}
