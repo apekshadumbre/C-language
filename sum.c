3e75e4f2d5e1c0a64ff605cc33b9b414c61ca4d7
apekshadumbre <93672377+apekshadumbre@users.noreply.github.com>
2026-04-01 14:38:30 +0530

    sum of numbers from 1 to 10 using for loop

---
+++ sum.c
@@ -0,0 +1,19 @@
+// for loop example sum of numbers from 1 to 10
+
+#include<stdio.h>
+int main()
+{
+	int i,sum=0;
+	
+	
+	
+	for(i=1;i<=10;i++)
+	{
+		sum=sum+i;
+		printf(" %d\n",i);
+		//sum++;
+	}
+	printf("Total sum of integer from 1 to 10 is: %d:",sum);
+	
+	return 0;
+}
